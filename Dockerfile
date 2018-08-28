347674ebed3bcf59ed8f138935debddbe7591c6f
dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2018-08-28 17:06:27 -0500

    Added flags for java memory

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN apt-get update && \
     && apt-get clean
 WORKDIR /aracneap
 COPY start.sh /aracneap/
+COPY lung-normal.tsv /aracneap/
+COPY genelist.txt /aracneap/
 CMD ["/aracneap/start.sh"]
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This is a comment!
 echo Hello World
-aws --version
+java -Xmx3892M -XX:-UseGCOverheadLimit -jar /opt/aracne/Aracne.jar -e lung-normal.tsv  -o /aracneap/output --tfs genelist.txt --pvalue 0 --seed 1 --calculateThreshold
+java -Xmx3892M -XX:-UseGCOverheadLimit -jar /opt/aracne/Aracne.jar -e lung-normal.tsv  -o /aracneap/output --tfs genelist.txt --pvalue 0 --seed 1
