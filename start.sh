d1c9defa2f026d72a29d07f8e0d926a3e5ea8115
dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2018-08-27 19:05:19 -0500

    Added s3 cp for bucket file

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # This is a comment!
 echo Hello World
-aws --version
+aws s3 cp s3://transregulation-aracne-inputs/Lung_Length.full_GC.full_Between.tmm_Arsyn_Normal input.txt
+ls
+head input.txt
