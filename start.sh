bb4e25b01b7983f50f87f08ab6d714d2ad733e28
Diana G <dgarcia@inmegen.edu.gob.mx>
2018-08-30 15:15:56 -0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is a comment!
 echo Hello World
-java -Xmx3892M -XX:-UseGCOverheadLimit -jar /opt/aracne/Aracne.jar -e lung-normal.tsv  -o /aracneap/output --tfs genelist.txt --pvalue 0 --seed 1 --calculateThreshold
-java -Xmx3892M -XX:-UseGCOverheadLimit -jar /opt/aracne/Aracne.jar -e lung-normal.tsv  -o /aracneap/output --tfs genelist.txt --pvalue 0 --seed 1
+java -Xmx15360M -XX:-UseGCOverheadLimit -jar /opt/aracne/Aracne.jar -e lung-normal.tsv  -o /aracneap/output --tfs genelist.txt --pvalue 0 --seed 1 --calculateThreshold
+java -Xmx15360M -XX:-UseGCOverheadLimit -jar /opt/aracne/Aracne.jar -e lung-normal.tsv  -o /aracneap/output --tfs genelist.txt --pvalue 0 --seed 1 --nodpi --nobootstrap --threads 6
